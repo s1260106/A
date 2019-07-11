9d67738fbe359a6a254818f26be027b69ad90861
Souki Abe <soukiabe@SoukinoMacBook-Air.local>
2019-07-11 20:53:58 +0900

    add third saikoro

--- saikoro.c
+++ saikoro.c
@@ -10,13 +10,15 @@ int main(){
   srand(time(NULL));
 int a=rand()%6+1;
 int b=rand()%6+1;
+ int c=rand()%6+1;
  printf("Rolling the dice...\n");
  printf("Die 1: %d\n",a);
  printf("Die 2: %d\n",b);
- printf("Total value: %d\n",a+b);
+ printf("Die 3: %d\n",c);
+ printf("Total value: %d\n%s",a+b,name);
  if((a+b)>7)
-   printf("You won!\n");
- else printf("You lost!\n");
+   printf(" won!\n");
+ else printf(" lost!\n");
 
  return 0;
 }
